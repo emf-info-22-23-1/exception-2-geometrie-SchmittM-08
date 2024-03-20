3f4651671c7ec23673447587165dfb04483d318e
schmittm <schmittm@WSTEMFA44-07.stu.net.fr.ch>
2024-03-20 13:32:55 +0100

    Exercice truc géométrie gestion exceptions

--- src/exercicegeometrieexception/ctrl/Ctrl.java
+++ src/exercicegeometrieexception/ctrl/Ctrl.java
@@ -11,25 +11,41 @@ public class Ctrl implements ICtrlIhm {
     public void demarrer() {
         refIhm.demarrer();
     }
-    
+
     @Override
-    public void selectCalcRadius( String value ) {
-        double valueDouble = Double.parseDouble( value );
-        refIhm.afficheResultatCircle( String.valueOf( refWorker.calcRadiusCircleFromArea( valueDouble ) ) );
+    public void selectCalcRadius(String value) {
+        try {
+            double valueDouble = Double.parseDouble(value);
+            refIhm.afficheResultatCircle(String.valueOf(refWorker.calcRadiusCircleFromArea(valueDouble)));
+        } catch (NumberFormatException e) {
+            refIhm.afficheMessage("Veuillez entrer une valeur valide pour l'aire du cercle");
+        }
     }
 
     @Override
-    public void selectCalcLargeur( String valueArea, String valueLongueur ) {
-        double valueAreaDouble = Double.parseDouble( valueArea );
-        double valueLongueurDouble = Double.parseDouble( valueLongueur );
-        refIhm.afficheResultatRectangle( String.valueOf( refWorker.calcLargeurFromRectangle( valueAreaDouble, valueLongueurDouble ) ) );
+    public void selectCalcLargeur(String valueArea, String valueLongueur) {
+        boolean areaValide = false;
+        try {
+            double valueAreaDouble = Double.parseDouble(valueArea);
+            areaValide = true;
+            double valueLongueurDouble = Double.parseDouble(valueLongueur);
+            refIhm.afficheResultatRectangle(String.valueOf(refWorker.calcLargeurFromRectangle(valueAreaDouble, valueLongueurDouble)));
+        } catch (NumberFormatException n) {
+            if (!areaValide) {
+                refIhm.afficheMessage("Veuillez entrer une valeur valide pour l'aire du rectangle (" + valueArea + " invalide)");
+            }else{
+                refIhm.afficheMessage("Veuillez entrer une valeur valide pour la longueur du rectangle (" + valueLongueur + " invalide)");
+            }
+        } catch (ArithmeticException a) {
+            refIhm.afficheMessage("Division par 0");
+        }
     }
 
-    public void setRefIhm( IIhmCtrl refIhm ) {
+    public void setRefIhm(IIhmCtrl refIhm) {
         this.refIhm = refIhm;
     }
 
-    public void setRefWorker( IWrkCtrl refWorker ) {
+    public void setRefWorker(IWrkCtrl refWorker) {
         this.refWorker = refWorker;
     }
 
